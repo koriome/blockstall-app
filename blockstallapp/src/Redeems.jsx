de5282ac88e2d0e6dbeef11a328b137906e1b7f5
David Miller <koriome504@gmail.com>
2023-06-04 20:33:31 -0400

    Update Redeems.jsx

--- blockstallapp/src/Redeems.jsx
+++ blockstallapp/src/Redeems.jsx
@@ -115,12 +115,6 @@ export default function Redeems({ session, parentGetUser }) {
             <div className="hero w-screen">
                 <div className="hero-content text-center">
                     <div className="w-full">
-                        <div className="alert alert-error shadow-lg">
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                <span>Blockstall is not live yet!! any redemptions made WILL NOT COUNT!</span>
-                            </div>
-                        </div>
                         <h1 className="text-5xl font-bold">Advancements</h1>
                         <p className="py-6">Welcome to where the magic happens! This is a list of every advancement in minecraft. Go to the one that you would like to see taken away from me, and use your hard-earned tokens to redeem it. To get you started, here are links to get information about advancements and acquire tokens. I'd also recommend using Crtl+F to search.</p>
                         <span>
